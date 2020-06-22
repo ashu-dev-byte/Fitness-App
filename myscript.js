1dfc71b0a3670f9135c43a8810a26b0fedbcb96e
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-06-23 02:40:02 +0530

    Minimal Edits

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 const conatiner = document.querySelector(".container");
+const marker = document.querySelector(".marker");
 const text = document.querySelector("#text");
 
 const totalTime = 7500;
@@ -9,17 +10,22 @@ breatheAnimation();
 
 function breatheAnimation() {
   text.innerHTML = "Breathe In!";
-
+  text.style.color = "green";
+  // marker.style.backgroundColor = "green";
   conatiner.className = "container grow";
 
   setTimeout(() => {
     text.innerHTML = "Hold!";
+    text.style.color = "orange";
+    // marker.style.backgroundColor = "orange";
 
     setTimeout(() => {
       text.innerHTML = "Breathe Out!";
+      text.style.color = "red";
+      // marker.style.backgroundColor = "red";
       conatiner.className = "container shrink";
-    }, holdTime);
-  }, breatheTime);
+    }, holdTime + 250);
+  }, breatheTime + 250);
 }
 
-setInterval(breatheAnimation, totalTime);
+setInterval(breatheAnimation, totalTime + 500);

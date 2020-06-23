105d6f578d18ff99a9934f147c59ab278cfd0b2c
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-06-24 03:20:27 +0530

    Added Comments

--- myscript.js
+++ myscript.js
@@ -12,17 +12,20 @@ function breatheAnimation() {
   text.innerHTML = "Breathe In!";
   text.style.color = "green";
   // marker.style.backgroundColor = "green";
+  // marker.style.backgroundColor = "#55b7a4";
   conatiner.className = "container-child-one grow";
 
   setTimeout(() => {
     text.innerHTML = "Hold!";
     text.style.color = "orange";
     // marker.style.backgroundColor = "orange";
+    // marker.style.backgroundColor = "white";
 
     setTimeout(() => {
       text.innerHTML = "Breathe Out!";
       text.style.color = "red";
       // marker.style.backgroundColor = "red";
+      // marker.style.backgroundColor = "#2a5b52";
       conatiner.className = "container-child-one shrink";
     }, holdTime);
   }, breatheTime);
